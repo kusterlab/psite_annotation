9ee12bbd505cd846b2d38ee6908af507e466a122
Matthew The <matthew.the@tum.de>
2024-02-21 16:14:45 +0100

    First readthedocs version

---
+++ docs/source/conf.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Psite annotation'
+copyright = '2024, Matthew The'
+author = 'Matthew The'
+release = '0.5.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+]
+
+autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. Psite annotation documentation master file, created by
+   sphinx-quickstart on Wed Feb 21 15:34:00 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Psite annotation's documentation!
+============================================
+
+.. toctree::
+   :hidden:
+   :maxdepth: 2
+   :caption: Contents:
+
+   Home page <self>
+   API reference <_autosummary/psite_annotation>
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
